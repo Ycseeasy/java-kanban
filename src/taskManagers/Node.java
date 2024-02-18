0969223ade50b6fafdef8a23800bb032d212ba6f
Ycseeasy <ycse2easyy@yandex.ru>
2024-02-18 13:35:51 +0300

    Добавил реализацию 6 ТЗ: Добавил дополнительный класс для узлов NODE

---
+++ src/taskManagers/Node.java
@@ -0,0 +1,17 @@
+package taskManagers;
+
+import tasks.Task;
+
+public class Node<Task> {
+
+    public Task task;
+    public Node<Task> next;
+    public Node<Task> prev;
+
+    public Node(Task task) {
+        this.task = task;
+        this.next = null;
+        this.prev = null;
+    }
+}
+
